2ad206a18d2f651de96948760e370e9a576b7c9b
shany <shany.razafindramary@epitech.eu>
2022-12-02 16:14:01 +0100

    [ADD] login fonctionality

--- symfony/src/Controller/ApiLoginController.php
+++ symfony/src/Controller/ApiLoginController.php
@@ -3,29 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(#[CurrentUser] ?User $user): Response
+    public function index(#[CurrentUser] ?UserInterface $user): Response
     {
-
+        
         if (null === $user) {
             return $this->json([
-                'message' => 'missing credentials',
-            ], Response::HTTP_UNAUTHORIZED);
+                'status' => 'error',
+                'message' => 'Utilisateur inconue',
+            ]);
         }
-
-        $token = "dvdgd";
         
         return $this->json([
-            'user'  => $user->getUserIdentifier(),
-            'token' => $token,
+            'status'  => 'success' ,
         ]);
     }
 }
