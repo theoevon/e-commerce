eb35081f426dc42086c40d1528bd088453f95f31
shany <shany.razafindramary@epitech.eu>
2022-12-14 13:42:20 +0100

    test

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,7 @@ import Tablette_article from './page/tablette_article.js';
 import Connexion from './page/connexion.js';
 import Inscription from './page/inscription.js';
 import User from './Admin.js';
-import PcPortable from './page/Pc_portable.js';
+import Ordinateur_Portable from './page/ordinateur_portable.jsx';
 import Tablette from './page/tablette.jsx';
 
 function App(){
@@ -15,11 +15,10 @@ function App(){
         <Route exact path='/' element={< Accueil />}></Route>
         <Route path='/tablette/:name' element={< Tablette_article />}></Route>
         <Route path='/tablette' element={< Tablette />}></Route>
-        <Route path='/ordinateur_portable' element={< PcPortable />}></Route>
+        <Route path='/ordinateur_portable' element={< Ordinateur_Portable />}></Route>
         <Route exact path='/connexion' element={<Connexion/>}></Route>
         <Route exact path='/inscription' element={<Inscription/>}></Route>
         <Route exact path='/admin/*' element={<User/>}></Route>
-        <Route exact path='/PC_portable' element={<PcPortable/>}></Route>
       </Routes>
     </Router>
   );
