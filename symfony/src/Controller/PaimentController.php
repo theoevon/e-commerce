da778736292733fd82e78e20c53b1436ba96be20
ilieselmarhouli <ilies.el-marhouli@epitech.eu>
2023-01-07 12:40:20 +0100

    frais de port

--- symfony/src/Controller/PaimentController.php
+++ symfony/src/Controller/PaimentController.php
@@ -26,27 +26,62 @@ class PaimentController extends AbstractController{
     }
     return $tableau;
   }
+  public function getFreeShippingFeez(){
+    
+    $jsonStr = file_get_contents('php://input');
+    $jsonObj = json_decode($jsonStr);
+    foreach ($jsonObj as $key => $value) {
+
+       $feez = ($value->variant[0]->weight * 0.8)*10;
+
+
+    return round($feez);
+  }
+}
+public function getShippingFeez(){
+    
+  $jsonStr = file_get_contents('php://input');
+  $jsonObj = json_decode($jsonStr);
+  foreach ($jsonObj as $key => $value) {
+
+     $feez = ((7+$value->variant[0]->weight)* 0.8)*18;
+
+  return round($feez);
+ 
+}
+}
+
 
  #[Route('/paiement', name: 'app_paiement')]
   public function paiment(Request $request): Response{
   $stripe =  new \Stripe\StripeClient('sk_test_51MMU68LjlqtzfHvan6obl4BUxY6qESoCb9f55yGlh8pyOlqV68komzdHvGw0kiI2LGvto5ANP6XlXImYw6cJlSdP00HGBapFpV');    
-   self::getProducts();
+   
    $session = $stripe->checkout->sessions->create(
     [
       'payment_method_types' => ['card'],
       'shipping_address_collection' => ['allowed_countries' => ['FR']],
       'shipping_options' => [
-        [
-          'shipping_rate_data' => [
-            'type' => 'fixed_amount',
-            'fixed_amount' => ['amount' => 0, 'currency' => 'eur'],
-            'display_name' => 'Free shipping',
-            'delivery_estimate' => [
-              'minimum' => ['unit' => 'business_day', 'value' => 5],
-              'maximum' => ['unit' => 'business_day', 'value' => 7],
-            ],
+        ['shipping_rate_data' => [
+          'type' => 'fixed_amount',
+          'fixed_amount' => ['amount' => self::getShippingFeez(), 'currency' => 'eur'],
+          'display_name' => 'Livraison express',
+          'delivery_estimate' => [
+            'minimum' => ['unit' => 'business_day', 'value' => 3],
+            'maximum' => ['unit' => 'business_day', 'value' => 4],
           ],
         ],
+      ],
+   ['shipping_rate_data' => [
+          'type' => 'fixed_amount',
+          'fixed_amount' => ['amount' => self::getFreeShippingFeez(), 'currency' => 'eur'],
+          'display_name' => 'Livraison standard',
+          'delivery_estimate' => [
+            'minimum' => ['unit' => 'business_day', 'value' => 10],
+            'maximum' => ['unit' => 'business_day', 'value' => 15],
+          ],
+        ],
+      ],
+
       ],
       'line_items' => [
        self::getProducts()
